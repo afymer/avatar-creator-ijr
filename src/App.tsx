1775bcfd5a792de4701b2978a76ba809c824c149
4paco <demange.telecom@gmail.com>
2025-04-05 18:14:24 +0200

    début de page 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import { useState } from 'react'
 import './style.css'
 
-function RoseDesVents() {
+function GoToBodiesPage() {
+    return <></>
+}
+
+function RoseDesVents_home_bg() {
     return (
         <img
-            className="opacity-30 fixed -bottom-1/100 md:-bottom-3/20 -left-2/5 md:-left-1/5 w-7xl md:w-2/3"
+            className="opacity-30 fixed -bottom-1/100 md:-bottom-3/20 -left-2/5 md:-left-1/5 w-7xl md:w-4/5"
             id="back_img"
             src="../images/rose_des_vents.png"
         ></img>
+    )
+}
+
+function RoseDesVents_avg_bg() {
+    return (
+        <div className="absolute w-full">
+            <img
+                className="translate-x-[50%] opacity-40 w-[80%] md:w-[50%]"
+                id="back_img"
+                src="../images/half_rose_des_vents.png"
+            ></img>
+        </div>
     )
 }
 
@@ -33,10 +49,17 @@ function MainTitle() {
     )
 }
 
-function SelectionButtons() {
+function SelectionButtons({
+    setCounter,
+}: {
+    setCounter: React.Dispatch<React.SetStateAction<number>>
+}) {
     return (
         <>
-            <button className="bg-button_bg border-2 border-amber-100 rounded-full p-[2%] relative md:m-[4%]  flex w-[7em] md:w-[11em] font-body ">
+            <button
+                className="bg-button_bg border-2 border-amber-100 rounded-full p-[2%] relative md:m-[4%]  flex w-[7em] md:w-[11em] font-body "
+                onClick={() => setCounter(1)}
+            >
                 <img
                     className="size-10 md:size-[10%] mr-4 ml-3 md:ml-0 mt-3 md:mt-0"
                     src="../images/plus.png"
@@ -49,16 +72,39 @@ function SelectionButtons() {
     )
 }
 
-function App() {
+function Instruction({ consigne }: { consigne: string }) {
     return (
-        <>
-            <RoseDesVents />
-            <div className="md:absolute text-amber-100 ml-15 md:ml-0 text-4xl top-1/100 right-1/6">
-                <MainTitle />
-                <SelectionButtons />
-            </div>
-        </>
+        <div className="font-title text-center text-amber-100 text-[200%] md:text-[250%] font-bold drop-shadow-[0_0_0.1em_rgba(72,2,65,1)] md:ml-5">
+            <h1 className="-top-45 text-5xl md:top-0 md:text-8xl relative font-title">
+                {consigne}
+            </h1>
+        </div>
     )
+}
+
+function App() {
+    const [page, setPage] = useState(0)
+
+    if (page === 0) {
+        return (
+            <>
+                <RoseDesVents_home_bg />
+                <div className="md:absolute text-amber-100 ml-15 md:ml-0 text-4xl top-1/100 right-1/6">
+                    <MainTitle />
+                    <SelectionButtons setCounter={setPage} />
+                </div>
+            </>
+        )
+    } else if (page === 1) {
+        return (
+            <>
+                <RoseDesVents_avg_bg />
+                <Instruction consigne={'Choose your class'} />
+            </>
+        )
+    }
+
+    return <></>
 }
 
 export default App
